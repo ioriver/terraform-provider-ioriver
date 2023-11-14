5e2747ec807a92040801a861e3ea0998094c2b58
Maayan Ben David <maayan@ioriver.io>
2023-11-14 09:45:15 +0200

    feat(terraform-provider-ioriver): Adding new release with updated docs

--- examples/resources/ioriver_behavior/resource.tf
+++ examples/resources/ioriver_behavior/resource.tf
@@ -4,16 +4,32 @@ resource "ioriver_behavior" "example_behavior" {
   path_pattern = "/static/*"
   actions = [
     {
-      type                 = "CACHE_BEHAVIOR"
-      cache_behavior_value = "CACHE"
+      cache_behavior = "CACHE"
     },
     {
-      type    = "CACHE_TTL"
-      max_ttl = "86400"
+      cache_ttl = 86400
     },
     {
-      type    = "BROWSER_CACHE_TTL"
-      max_ttl = "604800"
+      browser_cache_ttl = 120
     },
+    {
+      response_header = {
+        header_name  = "foo"
+        header_value = "bar"
+      }
+    },
+    {
+      cors_header = {
+        header_name  = "Access-Control-Allow-Origin"
+        header_value = "*"
+      }
+    },
+    {
+      status_code_cache = {
+        status_code    = "204"
+        cache_behavior = "CACHE"
+        cache_ttl      = 60
+      }
+    }
   ]
 }
