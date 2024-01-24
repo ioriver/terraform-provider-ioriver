974d1c9dc4b039ae8d59f8f8fa3f85207dd20228
Maayan Ben David <maayan@ioriver.io>
2024-01-24 11:27:26 +0200

    feat(terraform-provider-ioriver): Adding new release

--- start.sh
+++ start.sh
@@ -1,8 +1,10 @@
 export GOPROXY="https://proxy.golang.org,direct"
 export GOPRIVATE="github.com/ioriver"
 export GONOSUMDB="github.com/ioriver/*"
-go mod init github.com/ioriver/terraform-provider-ioriver
-go mod tidy
+#go mod init github.com/ioriver/terraform-provider-ioriver
+#go mod tidy
+#go get github.com/ioriver/terraform-provider-ioriver/internal/provider
+go get github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs
 go mod vendor
 go build ./...
 go test ./...
